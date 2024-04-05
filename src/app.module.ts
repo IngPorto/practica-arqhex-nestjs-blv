333a61f911c6de2d7144ce7d07e277618036a071
ingporto <ing.portocarrero@gmail.com>
2024-04-05 01:21:50 -0500

    eliminando import sobrante

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoreModule } from './core/core.module';
 import { InfraestructureModule } from './infraestructure/infraestructure.module';
 
 @Module({
-  imports: [ InfraestructureModule],
+  imports: [InfraestructureModule],
   controllers: [AppController],
   providers: [AppService],
 })
